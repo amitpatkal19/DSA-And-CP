b57018dc6a0be36deffb14ca250eb9ab4c87214e
AmitPatkal <amit.patkal19@vit.edu>
2022-02-20 23:45:57 +0530

    added MCM

---
+++ Dynamic Programming/MatricChainMultiplication.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int mem[100][100];
+int solve(int a[], int i, int j)
+{
+    if(i>=j)
+        return 0;
+    int min=INT_MAX;
+    for(int k=i;k<j;k++)
+    {
+        int temp = solve(a,i,k) + solve(a,k+1,j) + a[i-1]*a[k]*a[j];
+        if(min>temp)
+            min=temp;
+    }
+    return min;
+}
+
+int memo(int a[], int i, int j)
+{
+    if(i>=j)
+        return 0;
+    if(mem[i][j]!=-1)
+        return mem[i][j];
+    int min=INT_MAX;
+    int temp1,temp2;
+    for(int k=i;k<j;k++)
+    {
+        int temp = solve(a,i,k) + solve(a,k+1,j) + a[i-1]*a[k]*a[j];
+        if(min>temp)
+            min=temp;
+    }
+    return mem[i][j]=min;
+}
+int main()
+{
+    memset(mem,-1,sizeof(mem));
+    int a[5]={1,2,3,4,3};
+    cout<<solve(a,1,4)<<" "<<memo(a,1,4);
+}
